9eb353d0549abc7bc55fe3e6a53ad326044b46c2
PlayfulGod <playfulgod@gmail.com>
2013-02-09 04:07:37 -0500

    booting and wifi working again

--- device_warp2.mk
+++ device_warp2.mk
@@ -26,13 +26,11 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/prebuilts/root/init.warp2.rc:root/init.warp2.rc \
 	$(LOCAL_PATH)/prebuilts/root/init.2ndstg.rc:root/init.2ndstg.rc \
 	$(LOCAL_PATH)/prebuilts/root/logo.bmp:root/logo.bmp \
-	$(LOCAL_PATH)/prebuilts/root/init.qcom.rc:root/init.qcom.rc \
 	$(LOCAL_PATH)/prebuilts/root/init.qcom.sh:root/init.qcom.sh \
 	$(LOCAL_PATH)/prebuilts/root/init.qcom.usb.rc:root/init.qcom.usb.rc \
 	$(LOCAL_PATH)/prebuilts/root/init.qcom.usb.sh:root/init.qcom.usb.sh \
-	$(LOCAL_PATH)/prebuilts/root/init.rc:root/init.rc \
-	$(LOCAL_PATH)/prebuilts/root/ueventd.rc:root/ueventd.rc \
 	$(LOCAL_PATH)/prebuilts/root/sbin/usbconfig:root/sbin/usbconfig \
+	$(LOCAL_PATH)/prebuilts/root/ueventd.rc:root/ueventd.warp2.rc \
 	$(LOCAL_PATH)/prebuilts/root/init:root/init
 
 ## Recovery
@@ -52,12 +50,13 @@ PRODUCT_PROPERTY_OVERRIDES+= dalvik.vm.execution-mode=int:jit \
 	debug.enabletr=true \
 	persist.sys.use_dithering=0 \
 	ro.com.google.locationfeatures=1 \
-	mobiledata.interfaces = wlan0,rmnet0
+	mobiledata.interfaces = wlan0,rmnet0 \
+	ro.build.baseband_version=N861B01
 
 # Provides overrides to configure the Dalvik heap for a standard tablet device.
 PRODUCT_PROPERTY_OVERRIDES += \
-	dalvik.vm.heapstartsize=5m \
-	dalvik.vm.heapgrowthlimit=48m \
+	dalvik.vm.heapstartsize=8m
+	dalvik.vm.heapgrowthlimit=64m
 	dalvik.vm.heapsize=256m
 
 #LLVM for RenderScript
@@ -66,7 +65,6 @@ LLVM_ROOT_PATH := external/llvm
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
 	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
 	frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
 	frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
@@ -74,7 +72,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
 	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
 	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
 	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
 	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
 	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
@@ -86,14 +83,15 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
 	frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
+	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
 
 PRODUCT_PACKAGES += \
 	applypatch \
 	ast-mm-vdec-omx-test \
 	com.android.future.usb.accessory \
-	gps.default \
 	libdivxdrmdecrypt \
 	libinvensense_hal \
 	liblasic \
@@ -102,8 +100,8 @@ PRODUCT_PACKAGES += \
 	libmllite \
 	libmlplatform \
 	libmmjpeg_interface \
-	libmm-omxcore \
 	libmm-video \
+	libmm-omxcore \
 	libOmxAacEnc \
 	libOmxAmrEnc \
 	libOmxCore \
@@ -117,24 +115,25 @@ PRODUCT_PACKAGES += \
 	librs_jni \
 	libmedia_jni \
 	libstagefrighthw \
-	LiveWallpapers \
-	LiveWallpapersPicker \
-	make_ext4fs \
 	mm-vdec-omx-property-mgr \
 	mm-vdec-omx-test \
 	mm-venc-omx-test \
 	mm-venc-omx-test720p \
 	mm-video-driver-test \
-	mm-video-encdrv-test \
-	setup_fs \
-	liboverlay
+	mm-video-encdrv-test
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs
 
 # Display
 PRODUCT_PACKAGES += \
 	copybit.msm7x30 \
     gralloc.msm7x30 \
     hwcomposer.msm7x30 \
-	libgenlock
+	libgenlock \
+	liboverlay
 
 ## Bluetooth
 PRODUCT_PACKAGES += \
@@ -142,6 +141,13 @@ PRODUCT_PACKAGES += \
 	hciconfig \
 	hcitool \
 	hdmid
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/prebuilts/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
+	$(LOCAL_PATH)/prebuilts/etc/bluetooth/main.conf:system/etc/bluetooth/main.conf \
+    $(LOCAL_PATH)/prebuilts/etc/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
+    $(LOCAL_PATH)/prebuilts/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+	$(LOCAL_PATH)/prebuilts/etc/BCM4330B1_002.001.032.0518.0520.hcd:/system/etc/BCM4330.hcd
 
 # Keychar, Keylayout, & touchscreen
 PRODUCT_COPY_FILES += \
@@ -162,6 +168,18 @@ PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilts/etc/apns-conf.xml:system/etc/apns-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/etc/vold.fstab:system/etc/vold.fstab
 
+# GPS
+PRODUCT_PACKAGES += \
+	gps.default \
+	gps.msm7x30
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/etc/gps.conf:system/etc/gps.conf 
+#	$(LOCAL_PATH)/prebuilts/lib/libloc_adapter.so:system/lib/libloc_adapter.so \
+#	$(LOCAL_PATH)/prebuilts/lib/libloc_api-rpc-qc.so:system/lib/libloc_api-rpc-qc.so \
+#	$(LOCAL_PATH)/prebuilts/lib/libloc_eng.so:system/lib/libloc_eng.so \
+#	$(LOCAL_PATH)/prebuilts/lib/libloc_ext.so:system/lib/libloc_ext.so
+
 # Audio
 PRODUCT_PACKAGES += \
     audio_policy.msm7x30 \
@@ -170,17 +188,18 @@ PRODUCT_PACKAGES += \
 	libaudioutils
 
 PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/prebuilts/etc/audio_policy.conf:system/vendor/etc/audio_policy.conf \
 	$(LOCAL_PATH)/prebuilts/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
-	$(LOCAL_PATH)/prebuilts/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
-	$(LOCAL_PATH)/prebuilts/etc/audio_policy.conf:system/vendor/etc/audio_policy.conf
+	$(LOCAL_PATH)/prebuilts/lib/libaudioalsa.so:system/lib/libaudioalsa.so
+
 
 # Camera
 PRODUCT_PACKAGES += \
     LegacyCamera \
-    camera.warp2 \
+    camera.msm7x30 \
 	libcamera
 
-#PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/prebuilts/lib/liboemcamera.so:system/lib/liboemcamera.so \
 	$(LOCAL_PATH)/prebuilts/lib/liboemcamera.so:obj/lib/liboemcamera.so
 
@@ -193,36 +212,28 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/prebuilts/etc/wifi/p2p_supplicant.conf:system/etc/wifi/p2p_supplicant.conf \
-	$(LOCAL_PATH)/prebuilts/lib/libwidevine.so:system/lib/libwidevine.so \
     $(LOCAL_PATH)/prebuilts/etc/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    $(LOCAL_PATH)/prebuilts/etc/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
+    $(LOCAL_PATH)/prebuilts/etc/init.wlanprop.sh:system/etc/init.wlanprop.sh \
+    $(LOCAL_PATH)/prebuilts/etc/init.qcom.wifi.sh:system/etc/init.qcom.wifi.sh \
+    $(LOCAL_PATH)/prebuilts/etc/init.qcom.sdio.sh:system/etc/init.qcom.sdio.sh \
     $(LOCAL_PATH)/prebuilts/etc/firmware/fw_bcmdhd.bin:system/etc/firmware/fw_bcmdhd.bin \
     $(LOCAL_PATH)/prebuilts/etc/firmware/fw_bcmdhd_apsta.bin:system/etc/firmware/fw_bcmdhd_apsta.bin \
-    $(LOCAL_PATH)/prebuilts/etc/firmware/fw_bcmdhd_p2p.bin:system/etc/firmware/fw_bcmdhd_p2p.bin \
-    $(LOCAL_PATH)/prebuilts/bin/wpa_supplicant:system/bin/wpa_supplicant \
-    $(LOCAL_PATH)/prebuilts/bin/wpa_cli:system/bin/wpa_cli \
-	$(LOCAL_PATH)/prebuilts/bin/hostapd:system/bin/hostapd \
-	$(LOCAL_PATH)/prebuilts/bin/hostapd_cli:system/bin/hostapd_cli
+    $(LOCAL_PATH)/prebuilts/etc/firmware/fw_bcmdhd_p2p.bin:system/etc/firmware/fw_bcmdhd_p2p.bin
 
 PRODUCT_PACKAGES += \
 	libwpa_client
 	
 # Wireless AP
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
 	hostapd_cli \
 	hostapd
 
+# Extra packages
 PRODUCT_PACKAGES += \
-	Torch \
-	WiFiDirectDemo
-
-# Common Qualcomm scripts
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/etc/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
-    $(LOCAL_PATH)/prebuilts/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
-    $(LOCAL_PATH)/prebuilts/etc/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
-    $(LOCAL_PATH)/prebuilts/etc/init.wlanprop.sh:system/etc/init.wlanprop.sh \
-    $(LOCAL_PATH)/prebuilts/etc/init.qcom.wifi.sh:system/etc/init.qcom.wifi.sh \
-    $(LOCAL_PATH)/prebuilts/etc/init.qcom.sdio.sh:system/etc/init.qcom.sdio.sh
+	LiveWallpapers \
+	LiveWallpapersPicker \
+	Torch
 
 ## Media & Display Firmware
 PRODUCT_COPY_FILES += \
@@ -249,12 +260,16 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/etc/firmware/vidc_720p_mp4_dec_mc.fw:system/etc/firmware/vidc_720p_mp4_dec_mc.fw \
     $(LOCAL_PATH)/prebuilts/etc/firmware/vidc_720p_mp4_enc_mc.fw:system/etc/firmware/vidc_720p_mp4_enc_mc.fw \
     $(LOCAL_PATH)/prebuilts/etc/firmware/vidc_720p_vc1_dec_mc.fw:system/etc/firmware/vidc_720p_vc1_dec_mc.fw \
-	$(LOCAL_PATH)/prebuilts/etc/firmware/N970_N861_PR1183396_s2202_32313037.img:system/etc/firmware/N970_N861_PR1183396_s2202_32313037.img
+	$(LOCAL_PATH)/prebuilts/etc/media_profiles.xml:system/etc/media_profiles.xml \
+	$(LOCAL_PATH)/prebuilts/etc/media_codecs.xml:system/etc/media_codecs.xml
+
+# iptables
+PRODUCT_PACKAGES += \
+	ip6tables \
+	iptables
 
 # extra stuff
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilts/bin/ip6tables:system/bin/ip6tables \
-	$(LOCAL_PATH)/prebuilts/bin/iptables:system/bin/iptables \
 	$(LOCAL_PATH)/prebuilts/bin/cnd:system/bin/cnd \
 	$(LOCAL_PATH)/prebuilts/bin/akmd8962_new:system/bin/akmd8962_new \
 	$(LOCAL_PATH)/prebuilts/bin/rmt_storage:system/bin/rmt_storage
